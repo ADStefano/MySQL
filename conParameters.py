67ed5d2d0e925776d186e4b5069769ce39e7c1e0
Ângelo Di Stefano <85005635+ADStefano@users.noreply.github.com>
2022-05-05 22:26:09 -0300

    Update conParameters.py

--- conParameters.py
+++ conParameters.py
@@ -1,5 +1,5 @@
 host = "localhost" # As informações podem variar de acordo com as configurações do banco de dados
-usuario = "root" 
+usuario = "root" # usuário e senha padrôes
 senha = "root"
 db = "ConexaoPython"
 port = 3306
